1428987d2033ba8c1662f680471df066f833bc07
jinheumkim <kjjppwl510@naver.com>
2025-07-07 21:04:38 +0900

    fix: userAccount, userConnection 양방향 매핑

--- src/main/java/me/jinheum/datelog/Entity/UserAccount.java
+++ src/main/java/me/jinheum/datelog/Entity/UserAccount.java
@@ -4,8 +4,10 @@ import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,4 +31,10 @@ public class UserAccount {
     private String email;
 
     private String password;
+
+    @OneToOne(mappedBy = "user", fetch = FetchType.LAZY)
+    private UserConnection userconnection;
+
+    @OneToOne(mappedBy = "partner", fetch = FetchType.LAZY)
+    private UserConnection partnerConnection;
 }
